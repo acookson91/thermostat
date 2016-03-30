0ad75d42d180038bea6f39d838100b81d580656a
Joseph Sharratt <joseph.sharratt@gmail.com>
2016-03-30 16:37:05 +0100

    Back end

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -3,7 +3,7 @@ describe('Thermostat', function() {
   var thermostat;
 
   beforeEach(function(){
-    thermostat = new Thermostat();
+    thermostat = new Thermostat(20);
   });
 
   it("Starts with 20", function(){
@@ -12,12 +12,12 @@ describe('Thermostat', function() {
 
   it('increments temp by 1', function(){
     thermostat.increaseTemp();
-    expect(thermostat.showTemp()).toEqual(21);
+    expect(thermostat.showTemp()).toEqual(thermostat.startTemp+=1);
   });
 
   it('decrement temp by 1', function(){
     thermostat.decreaseTemp();
-    expect(thermostat.showTemp()).toEqual(19);
+    expect(thermostat.showTemp()).toEqual(thermostat.startTemp-=1);
   });
 
   it('cannot be decremented below minimum temp', function(){
@@ -41,4 +41,39 @@ describe('Thermostat', function() {
       expect(function() {thermostat.increaseTemp()
       }).toThrowError('sweaty');
     });
+
+    it('power saving mode reduces max temperature', function() {
+      thermostat.powerSave();
+      thermostat.powerSave();
+      expect(thermostat.max).toEqual(25)
+    })
+
+    it('switching power mode off increases max temperature', function(){
+      thermostat.powerSave();
+      expect(thermostat.max).toEqual(32)
+    })
+
+    it('reset button resets temperature to 20', function(){
+      thermostat.reset();
+      expect(thermostat.currentTemp).toEqual(thermostat.startTemp)
+    });
+
+    it('colour displays green if current temp is below 18', function(){
+      thermostat.currentTemp = 17
+      thermostat.setColour
+      expect(thermostat.colour).toEqual('green')
+    })
+
+    it('colour displays red if current temp is above 25', function(){
+      thermostat.currentTemp = 26
+      thermostat.setColour()
+      expect(thermostat.colour).toEqual('red')
+    })
+
+    it('otherwise colour displays yellow', function(){
+      thermostat.currentTemp = 20
+      thermostat.setColour()
+      expect(thermostat.colour).toEqual('yellow')
+    })
+
   });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,23 +1,48 @@
-function Thermostat() {
-  this.temp = 20
+function Thermostat(start) {
+  this.startTemp = start
+  this.currentTemp = this.startTemp
   this.min = 10
-  this.max = 32
+  this.max = 25
+  this.isPowerSaveOn = true
+  this.colour = 'green'
+  // this.currentTemp < 18 ? this.colour = 'green' : this.currentTemp > 25 ? this.colour = 'red' : this.colour = 'yellow'
+  // this.colour = (this.currentTemp < 18 ? 'green' : this.currentTemp > 25 ? 'red' : 'yellow')
+
 }
 
   Thermostat.prototype.showTemp = function() {
-    return this.temp
+    return this.currentTemp
   };
 
   Thermostat.prototype.increaseTemp = function() {
-    if (this.temp >= this.max) {
+    if (this.currentTemp >= this.max) {
       throw new Error('sweaty')
     } else {
-    this.temp++ }
+    this.currentTemp++ }
   };
 
   Thermostat.prototype.decreaseTemp = function() {
-    if (this.temp <= this.min) {
+    if (this.currentTemp <= this.min) {
       throw new Error('nah mate too cold')
     } else {
-    this.temp-- }
+    this.currentTemp-- }
+  };
+
+  Thermostat.prototype.powerSave = function() {
+    this.isPowerSaveOn ? this.max += 7 : this.max -=7
+    this.isPowerSaveOn ^= true
+  };
+
+  Thermostat.prototype.reset = function() {
+    this.currentTemp = this.startTemp
+  };
+
+  Thermostat.prototype.setColour = function() {
+    if (this.currentTemp > 25) {
+      this.colour = 'red'
+    } else if (this.currentTemp > 18) {
+      this.colour = 'yellow'
+    } else {
+      this.colour = 'green'
+    }
   };
